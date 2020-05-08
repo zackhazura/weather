449b388f77c305f6e76f931e4022dff3e82aea3c
Zack Singh <zacharysingh@gmail.com>
2020-05-08 14:50:47 -0700

    Create reportWeather function

--- app/functions.js
+++ app/functions.js
@@ -1,11 +1,22 @@
 const axios = require('axios')
-const API_KEY = require('./API_KEY.js').API_KEY
+const API_KEY = require('./config/API_KEY.js').API_KEY
 
 const modelLocations = () => {
   const args = process.argv.slice(2).join(' ').split(', ')
   return args
 }
 
+const reportWeather = (locations, getCallback, logCallback) => {
+  if (locations.length === 1 && locations[0] === '') {
+    console.log('Error: No location argument(s) given')
+  } else {
+    locations.forEach(location => {
+      getCallback(location, logCallback)
+    })
+  }
+}
+
 module.exports = {
-  modelLocations
+  modelLocations,
+  reportWeather
 }

b0b11f4190272f4114f1f3dcaba1a37192f46e73
Zack Singh <zacharysingh@gmail.com>
2020-05-08 14:45:17 -0700

    Configure initial test file appSpec.js

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -4,26 +4,5 @@ const functions = require('../app/functions')
 
 describe('Weather App', function(){
   
-  const reportWeather = functions.reportWeather
-  const getWeatherFromAPI = functions.getWeatherFromAPI
-  const logResultToConsole = functions.logResultToConsole
-  const modelLocations = functions.modelLocations
 
-  describe('modelLocations()', function() {
-
-    it('should return an array', function(){
-      const result = modelLocations()
-      expect(result).to.be.an('array')
-    })
-
-    it('should produce an array of strings', function(){
-      let areStrings = true
-      const result = modelLocations()
-      for (let i = 0; i < result.length; i++) {
-        if (typeof result[i] !== 'string') {
-          areStrings = false
-        }
-      }
-      expect(areStrings).to.be.true
-    })
-  })
+})

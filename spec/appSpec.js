0dcd52475fc5cdf0872d224908e4188fe31adab2
Zack Singh <zacharysingh@gmail.com>
2020-05-08 14:46:07 -0700

    Create tests for modelLocations function

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -3,6 +3,25 @@ const sinon = require('sinon')
 const functions = require('../app/functions')
 
 describe('Weather App', function(){
-  
+  const modelLocations = functions.modelLocations
+
+  describe('modelLocations()', function() {
+
+    it('should return an array', function(){
+      const result = modelLocations()
+      expect(result).to.be.an('array')
+    })
+
+    it('should produce an array of strings', function(){
+      let areStrings = true
+      const result = modelLocations()
+      for (let i = 0; i < result.length; i++) {
+        if (typeof result[i] !== 'string') {
+          areStrings = false
+        }
+      }
+      expect(areStrings).to.be.true
+    })
+  })
 
 })
